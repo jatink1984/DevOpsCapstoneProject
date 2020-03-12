604d8fee41c53b57da3ae7a4212e52c75a4ec797
j257305 <jatin.kumar@8451.com>
2020-03-12 19:03:24 -0400

    Jatin|update jenkins file

--- createDeployment.sh
+++ createDeployment.sh
@@ -6,12 +6,12 @@ if [ $CURRENT == "blue" ]
 then
  echo "blue is live, create new green deployment."
  ssh ec2-user@ec2-3-133-144-139.us-east-2.compute.amazonaws.com kubectl apply -f green-deployment.yml
-# READY=$(ssh ec2-user@ec2-3-133-144-139.us-east-2.compute.amazonaws.com kubectl get deploy green -o json | ssh ec2-user@ec2-3-133-144-139.us-east-2.compute.amazonaws.com jq '.status.conditions[] | select(.reason == "MinimumReplicasAvailable") | .status' | tr -d '"')
-# while [[ "$READY" != "True" ]]; do
-#   READY=$(ssh ec2-user@ec2-3-133-144-139.us-east-2.compute.amazonaws.com kubectl get deploy green -o json | ssh ec2-user@ec2-3-133-144-139.us-east-2.compute.amazonaws.com jq '.status.conditions[] | select(.reason == "MinimumReplicasAvailable") | .status' | tr -d '"')
-#   sleep 2
-# done
-# ssh ec2-user@ec2-3-133-144-139.us-east-2.compute.amazonaws.com kubectl patch svc bluegreenlb -p '{\"spec\":{\"selector\": {\"app\": \"green\"}}}'
+ READY=$(ssh ec2-user@ec2-3-133-144-139.us-east-2.compute.amazonaws.com kubectl get deploy green -o json | ssh ec2-user@ec2-3-133-144-139.us-east-2.compute.amazonaws.com jq '.status.conditions[] | select(.reason == "MinimumReplicasAvailable") | .status' | tr -d '"')
+ while [[ "$READY" != "True" ]]; do
+   READY=$(ssh ec2-user@ec2-3-133-144-139.us-east-2.compute.amazonaws.com kubectl get deploy green -o json | ssh ec2-user@ec2-3-133-144-139.us-east-2.compute.amazonaws.com jq '.status.conditions[] | select(.reason == "MinimumReplicasAvailable") | .status' | tr -d '"')
+   sleep 2
+ done
+ ssh ec2-user@ec2-3-133-144-139.us-east-2.compute.amazonaws.com kubectl patch svc bluegreenlb -p '{\"spec\":{\"selector\": {\"app\": \"green\"}}}'
  echo "Green is Live."
  ssh ec2-user@ec2-3-133-144-139.us-east-2.compute.amazonaws.com kubectl delete deployment blue
 elif [ $CURRENT == "green" ]
